99408fdc7ffcc43deca033d54fab39cd39325dd1
aegis <aegis@cb5fa4c3-b70d-0410-b4f6-90f5898e5689>
2002-05-16 19:13:48 +0000

    make win32 timer threadsafe
    git-svn-id: http://audiere.svn.sourceforge.net/svnroot/audiere/trunk@166 cb5fa4c3-b70d-0410-b4f6-90f5898e5689

--- src/timer_win32.cpp
+++ src/timer_win32.cpp
@@ -3,23 +3,13 @@
 #include "timer.hpp"
 
 
-static adr_u64 s_frequency;
-
-
 adr_u64 GetNow() {
-  // do we have a known frequency yet?
-  if (s_frequency == 0) {
-    LARGE_INTEGER frequency;
-    if (QueryPerformanceFrequency(&frequency)) {
-      s_frequency = frequency.QuadPart;
-    }
-  }
-
-  // if we have a frequency, try to use the performance counter
-  if (s_frequency != 0) {
+  // get frequency of the performance counter
+  LARGE_INTEGER frequency;
+  if (QueryPerformanceFrequency(&frequency) && frequency.QuadPart != 0) {
     LARGE_INTEGER now;
     if (QueryPerformanceCounter(&now)) {
-      return 1000000 * adr_u64(now.QuadPart) / s_frequency;
+      return 1000000 * adr_u64(now.QuadPart) / frequency.QuadPart;
     }
   }
 

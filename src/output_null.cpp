1a2b9bc11ceb9e735a71d9cb258b872abb32f9ee
aegis <aegis@cb5fa4c3-b70d-0410-b4f6-90f5898e5689>
2002-05-17 20:56:01 +0000

    in theory, the null output driver should do the right thing wrt timing now...  it's a nontrivial amount of code, not exactly a _null_ output driver ;)
    git-svn-id: http://audiere.svn.sourceforge.net/svnroot/audiere/trunk@169 cb5fa4c3-b70d-0410-b4f6-90f5898e5689

--- src/output_null.cpp
+++ src/output_null.cpp
@@ -1,6 +1,30 @@
-#include "output_null.hpp"
-#include "threads.hpp"
+#ifdef _MSC_VER
+#pragma warning(disable : 4786)
+#endif
 
+
+#include <algorithm>
+#include <functional>
+#include "output_null.hpp"
+#include "input.hpp"
+#include "timer.hpp"
+#include "threads.hpp"
+#include "utility.hpp"
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+NullOutputContext::NullOutputContext()
+{
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+NullOutputContext::~NullOutputContext()
+{
+  ADR_ASSERT(m_streams.size() == 0,
+    "Null output context should not die with open streams");
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -15,7 +39,21 @@ NullOutputContext::Initialize(const char* /*parameters*/)
 void
 NullOutputContext::Update()
 {
-  AI_Sleep(100);
+  /*
+    UGH.  We can't use VC6 here because you can't use mem_fun with void
+    functions.  :(
+  std::for_each(
+    m_streams.begin(),
+    m_streams.end(),
+    std::mem_fun(&NullOutputStream::Update));
+  */
+
+  StreamList::iterator i = m_streams.begin();
+  for (; i != m_streams.end(); ++i) {
+    (*i)->Update();
+  }
+
+  AI_Sleep(50);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -23,14 +61,37 @@ NullOutputContext::Update()
 IOutputStream*
 NullOutputContext::OpenStream(ISampleSource* source)
 {
-  return new NullOutputStream();
+  NullOutputStream* stream = new NullOutputStream(this, source);
+  m_streams.insert(stream);
+  return stream;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-NullOutputStream::NullOutputStream()
-: m_volume(ADR_VOLUME_MAX)
+void
+NullOutputContext::RemoveStream(NullOutputStream* stream) {
+  m_streams.erase(stream);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+NullOutputStream::NullOutputStream(
+  NullOutputContext* context,
+  ISampleSource* source)
+: m_context(context)
+, m_source(source)
+, m_is_playing(false)
+, m_volume(ADR_VOLUME_MAX)
+, m_last_update(0)
 {
+  m_source->GetFormat(m_channel_count, m_sample_rate, m_bits_per_sample);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+NullOutputStream::~NullOutputStream()
+{
+  m_context->RemoveStream(this);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -38,6 +99,8 @@ NullOutputStream::NullOutputStream()
 void
 NullOutputStream::Play()
 {
+  m_is_playing = true;
+  ResetTimer();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -45,6 +108,7 @@ NullOutputStream::Play()
 void
 NullOutputStream::Stop()
 {
+  m_is_playing = false;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -52,13 +116,7 @@ NullOutputStream::Stop()
 void
 NullOutputStream::Reset()
 {
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-void
-NullOutputStream::ResetInputAndOutput()
-{
+  ResetTimer();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -66,7 +124,7 @@ NullOutputStream::ResetInputAndOutput()
 bool
 NullOutputStream::IsPlaying()
 {
-  return false;
+  return m_is_playing;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -83,6 +141,60 @@ int
 NullOutputStream::GetVolume()
 {
   return m_volume;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void
+NullOutputStream::ResetTimer()
+{
+  m_last_update = GetNow();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void
+NullOutputStream::Update()
+{
+  if (m_is_playing) {
+    // get number of milliseconds elapsed since last playing update
+    // so we can read that much time worth of samples
+    adr_u64 now = GetNow();
+    adr_u64 elapsed = now - m_last_update;
+
+    int samples_to_read = m_sample_rate * elapsed / 1000000;
+    int samples_read = DummyRead(samples_to_read);
+
+    if (samples_read != samples_to_read) {
+      m_source->Reset();
+      Stop();
+    }
+
+    m_last_update = now;
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+int
+NullOutputStream::DummyRead(int samples_to_read)
+{
+  int total = 0;  // number of samples read so far
+
+  // read samples into dummy buffer, counting the number we actually read
+  adr_u8* dummy = new adr_u8[1024 * m_channel_count * m_bits_per_sample / 8];
+  while (samples_to_read > 0) {
+    int read = adr_max(1024, samples_to_read);
+    int actual_read = m_source->Read(read, dummy);
+    total += actual_read;
+    samples_to_read -= actual_read;
+    if (actual_read < read) {
+      break;
+    }
+  }
+
+  delete[] dummy;
+  return total;
 }
 
 ////////////////////////////////////////////////////////////////////////////////

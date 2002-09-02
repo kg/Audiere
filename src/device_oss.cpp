6e1673d96481dbc13be4ec21520fa7223c3bd46f
aegis <aegis@cb5fa4c3-b70d-0410-b4f6-90f5898e5689>
2002-09-02 19:57:16 +0000

    update Cygwin build (specifically the OSS output device) to use the MixerDevice
    git-svn-id: http://audiere.svn.sourceforge.net/svnroot/audiere/trunk@300 cb5fa4c3-b70d-0410-b4f6-90f5898e5689

--- src/device_oss.cpp
+++ src/device_oss.cpp
@@ -6,7 +6,6 @@
 #include <sys/ioctl.h>
 #include <sys/soundcard.h>
 #include "device_oss.h"
-#include "buffer_stream.h"
 #include "debug.h"
 
 
@@ -57,9 +56,10 @@ namespace audiere {
   }
 
 
-  OSSAudioDevice::OSSAudioDevice(int output_device) {
+  OSSAudioDevice::OSSAudioDevice(int output_device)
+    : MixerDevice(44100)
+  {
     m_output_device = output_device;
-    m_mixer = new Mixer(44100);
   }
 
 
@@ -86,139 +86,13 @@ namespace audiere {
     while (sample_count > 0) {
       int transfer_count = std::min(sample_count, BUFFER_SIZE);
 
-      m_mixer->read(transfer_count, buffer);
+      read(transfer_count, buffer);
       write(m_output_device, buffer, transfer_count * 4);
 
       sample_count -= transfer_count;
     }
 
     usleep(50000);  // 50 milliseconds
-  }
-
-
-  OutputStream*
-  OSSAudioDevice::openStream(SampleSource* source) {
-    return (source ? new OSSOutputStream(this, source) : 0);
-  }
-
-
-  OutputStream*
-  OSSAudioDevice::openBuffer(
-    void* samples, int sample_count,
-    int channel_count, int sample_rate, SampleFormat sample_format)
-  {
-    return openStream(new BufferStream(
-      samples, sample_count,
-      channel_count, sample_rate, sample_format));
-  }
-
-
-  OSSOutputStream::OSSOutputStream(
-    OSSAudioDevice* device,
-    SampleSource* source)
-  {
-    m_device = device;
-    m_source = source;
-
-    getMixer().addSource(m_source);
-  }
-
-
-  OSSOutputStream::~OSSOutputStream() {
-    getMixer().removeSource(m_source);
-  }
-
-
-  void
-  OSSOutputStream::play() {
-    getMixer().setPlaying(m_source, true);
-  }
-
-
-  void
-  OSSOutputStream::stop() {
-    getMixer().setPlaying(m_source, false);
-  }
-
-
-  bool
-  OSSOutputStream::isPlaying() {
-    return getMixer().isPlaying(m_source);
-  }
-
-
-  void
-  OSSOutputStream::reset() {
-    getMixer().resetSource(m_source);
-  }
-
-
-  void
-  OSSOutputStream::setRepeat(bool repeat) {
-    getMixer().setRepeat(m_source, repeat);
-  }
-
-
-  bool
-  OSSOutputStream::getRepeat() {
-    return getMixer().getRepeat(m_source);
-  }
-
-
-  void
-  OSSOutputStream::setVolume(float volume) {
-    getMixer().setVolume(m_source, volume);
-  }
-
-
-  float
-  OSSOutputStream::getVolume() {
-    return getMixer().getVolume(m_source);
-  }
-
-
-  void
-  OSSOutputStream::setPan(float pan) {
-    getMixer().setPan(m_source, pan);
-  }
-
-
-  float
-  OSSOutputStream::getPan() {
-    return getMixer().getPan(m_source);
-  }
-
-
-  bool
-  OSSOutputStream::isSeekable() {
-    /// @todo  implement OSSOutputStream::isSeekable
-    return false;
-  }
-
-
-  int
-  OSSOutputStream::getLength() {
-    /// @todo  implement OSSOutputStream::getLength
-    return 0;
-  }
-
-
-  void
-  OSSOutputStream::setPosition(int position) {
-    /// @todo  implement OSSOutputStream::setPosition
-  }
-
-
-  int
-  OSSOutputStream::getPosition() {
-    /// @todo  implement OSSOutputStream::getPosition
-    return 0;
-  }
-
-
-  Mixer&
-  OSSOutputStream::getMixer() {
-    return *(m_device->m_mixer);
   }
 
 }
--- src/device_oss.h
+++ src/device_oss.h
@@ -3,12 +3,12 @@
 
 
 #include "audiere.h"
-#include "mixer.h"
+#include "device_mixer.h"
 
 
 namespace audiere {
 
-  class OSSAudioDevice : public RefImplementation<AudioDevice> {
+  class OSSAudioDevice : public MixerDevice {
   public:
     static OSSAudioDevice* create(const ParameterList& parameters);
 
@@ -18,49 +18,9 @@ namespace audiere {
 
   public:
     void update();
-    OutputStream* openStream(SampleSource* source);
-    OutputStream* openBuffer(
-      void* samples, int sample_count,
-      int channel_count, int sample_rate, SampleFormat sample_format);
 
   private:
     int m_output_device;
-    RefPtr<Mixer> m_mixer;
-
-    friend class OSSOutputStream;
-  };
-
-
-  class OSSOutputStream : public RefImplementation<OutputStream> {
-  private:
-    OSSOutputStream(OSSAudioDevice* device, SampleSource* source);
-    ~OSSOutputStream();
-
-  public:
-    void play();
-    void stop();
-    bool isPlaying();
-    void reset();
-
-    void setRepeat(bool repeat);
-    bool getRepeat();
-    void  setVolume(float volume);
-    float getVolume();
-    void  setPan(float pan);
-    float getPan();
-
-    bool isSeekable();
-    int  getLength();
-    void setPosition(int position);
-    int  getPosition();
-
-  private:
-    Mixer& getMixer();
-
-    RefPtr<OSSAudioDevice> m_device;
-    SampleSource* m_source;
-
-    friend class OSSAudioDevice;
   };
 
 }

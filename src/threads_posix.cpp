fbce018bf9540c42e2f543be9398783d3a699e66
aegis <aegis@cb5fa4c3-b70d-0410-b4f6-90f5898e5689>
2003-07-17 21:39:32 +0000

    made thread priority work when using pthreads
    git-svn-id: http://audiere.svn.sourceforge.net/svnroot/audiere/trunk@562 cb5fa4c3-b70d-0410-b4f6-90f5898e5689

--- src/threads_posix.cpp
+++ src/threads_posix.cpp
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <pthread.h>
 #include "threads.h"
+#include "utility.h"
 
 
 namespace audiere {
@@ -29,14 +30,51 @@ namespace audiere {
     ti->routine = routine;
     ti->opaque  = opaque;
 
-    pthread_t thread;
-    int result = pthread_create(&thread, NULL, ThreadRoutine, ti);
-    if (result != 0) {
+    pthread_attr_t attr;
+    if (pthread_attr_init(&attr)) {
       delete ti;
       return false;
-    } else {
-      return true;
     }
+
+    // get default scheduling parameters
+    int policy;
+    if (pthread_attr_getschedpolicy(&attr, &policy)) {
+      pthread_attr_destroy(&attr);
+      delete ti;
+      return false;
+    }
+
+    int min_prio = sched_get_priority_min(policy);
+    int max_prio = sched_get_priority_max(policy);
+
+    sched_param sched;
+    if (pthread_attr_getschedparam(&attr, &sched)) {
+        pthread_attr_destroy(&attr);
+        delete ti;
+        return false;
+    }
+
+    sched.sched_priority = clamp(
+        min_prio,
+        sched.sched_priority + priority,
+        max_prio);
+
+    if (pthread_attr_setschedparam(&attr, &sched)) {
+        pthread_attr_destroy(&attr);
+        delete ti;
+        return false;
+    }
+    
+    pthread_t thread;
+    int result = pthread_create(&thread, &attr, ThreadRoutine, ti);
+    if (result != 0) {
+      pthread_attr_destroy(&attr);
+      delete ti;
+      return false;
+    }
+
+    pthread_attr_destroy(&attr);
+    return true;
   }
 
 

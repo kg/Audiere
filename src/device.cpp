53dedfa514ebf50eb291c07016de688ecb051c1c
sunmoon1997 <sunmoon1997@cb5fa4c3-b70d-0410-b4f6-90f5898e5689>
2008-12-21 05:56:28 +0000

    Add a CoreAudio device driver. As a result, audiere works on Mac OS X now.:-)
    git-svn-id: http://audiere.svn.sourceforge.net/svnroot/audiere/trunk@691 cb5fa4c3-b70d-0410-b4f6-90f5898e5689

--- src/device.cpp
+++ src/device.cpp
@@ -44,6 +44,9 @@
   #include "device_pa.h"
 #endif
 
+#ifdef HAVE_CORE_AUDIO
+  #include "device_coreaudio.h"
+#endif
 
 namespace audiere {
 
@@ -175,6 +178,9 @@ namespace audiere {
 #ifdef HAVE_PA
       "pa:portaudo compatible"  ";"
 #endif
+#ifdef HAVE_CORE_AUDIO
+      "coreaudio:Core Audio (Mac OS X)"  ";"
+#endif
 
 #endif
       "null:Null output (no sound)"  ;
@@ -236,7 +242,8 @@ namespace audiere {
         TRY_GROUP("directsound");
         TRY_GROUP("winmm");
         TRY_GROUP("oss");
-	TRY_GROUP("portaudio");	
+        TRY_GROUP("portaudio");
+        TRY_GROUP("coreaudio");
         return 0;
       }
 
@@ -274,12 +281,21 @@ namespace audiere {
           return 0;
         }
       #endif
+
       #ifdef HAVE_PA
 	if (name == "portaudio") {
           TRY_DEVICE(PAAudioDevice);
           return 0;
 	}
       #endif
+
+      #ifdef HAVE_CORE_AUDIO
+	if (name == "coreaudio") {
+          TRY_DEVICE(CAAudioDevice);
+          return 0;
+	}
+      #endif
+
       if (name == "null") {
         TRY_DEVICE(NullAudioDevice);
         return 0;
